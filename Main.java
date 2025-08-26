d358665e0f30a2ee36fc2a9d39921f5ba3f43441
Bo-MacDonald <bomac0927@gmail.com>
2025-08-25 21:14:13 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SuperBowl sbLIII = new SuperBowl("Feb 3, 2019", "LIII", "Patriots", 3.2, "Rams", 4.1);
+        SuperBowl sbLIII = new SuperBowl("Feb 3, 2019", "LIII", "Patriots", 6.0, "Rams", 4.3);
 
         System.out.println("Average yards per play in the game: " + sbLIII.findAVGOfGame());
         sbLIII.compareTeams();
