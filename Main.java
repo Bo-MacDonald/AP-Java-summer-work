35690038faaaab7ba4ec7eb813c14a768a1c8491
Bo-MacDonald <bomac0927@gmail.com>
2025-09-07 19:08:05 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Example of SuperBowl object
         SuperBowl sbLIII = new SuperBowl("Feb 3, 2019", "LIII", "Patriots", 6.0, "Rams", 4.3);
 
         System.out.println("Average yards per play in the game: " + sbLIII.findAVGOfGame());
